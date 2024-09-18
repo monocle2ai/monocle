987877df107a2610403c755a67f7fc3618173a4a
Prasad Mujumdar <prasad@okahu.ai>
2024-09-18 13:41:14 -0700

    Updated README and moved the map files
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/langchain/__init__.py
+++ src/monocle_apptrace/langchain/__init__.py
@@ -3,4 +3,4 @@ from monocle_apptrace.utils import load_wrapper_from_config
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 LANGCHAIN_METHODS = load_wrapper_from_config(
-    os.path.join(parent_dir, 'wrapper_config', 'lang_chain_methods.json'))
+    os.path.join(parent_dir, 'metamodel', 'maps', 'lang_chain_methods.json'))

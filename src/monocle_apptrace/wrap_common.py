1b181247c26c757a9c503c68d57b21d00472c71c
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2024-10-16 10:07:25 +0530

    prevent exception
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/monocle_apptrace/wrap_common.py
+++ src/monocle_apptrace/wrap_common.py
@@ -298,9 +298,9 @@ def update_workflow_type(to_wrap, span: Span):
 def update_span_with_context_input(to_wrap, wrapped_args, span: Span):
     package_name: str = to_wrap.get('package')
     input_arg_text = ""
-    if "langchain_core.retrievers" in package_name:
+    if "langchain_core.retrievers" in package_name and len(wrapped_args) > 0:
         input_arg_text += wrapped_args[0]
-    if "llama_index.core.indices.base_retriever" in package_name:
+    if "llama_index.core.indices.base_retriever" in package_name and len(wrapped_args) > 0:
         input_arg_text += wrapped_args[0].query_str
     if "haystack.components.retrievers.in_memory" in package_name:
         input_arg_text += get_attribute(CONTEXT_INPUT_KEY)
@@ -313,7 +313,7 @@ def update_span_with_context_output(to_wrap, return_value, span: Span):
         output_arg_text += " ".join([doc.page_content for doc in return_value if hasattr(doc, 'page_content')])
         if len(output_arg_text) > 100:
             output_arg_text = output_arg_text[:100] + "..."
-    if "llama_index.core.indices.base_retriever" in package_name:
+    if "llama_index.core.indices.base_retriever" in package_name and len(return_value) > 0:
         output_arg_text += return_value[0].text
     if "haystack.components.retrievers.in_memory" in package_name:
         output_arg_text += " ".join([doc.content for doc in return_value['documents']])

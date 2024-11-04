d41e4c53938524d3031ba7bd02fd291f037b2189
sachintendulkar576123 <sachin.tendulkar@beehyv.com>
2024-11-04 18:58:37 +0530

    update haystack metamodel
    Signed-off-by: sachintendulkar576123 <sachin.tendulkar@beehyv.com>

--- src/monocle_apptrace/wrap_common.py
+++ src/monocle_apptrace/wrap_common.py
@@ -454,6 +454,8 @@ def update_span_with_prompt_output(to_wrap, wrapped_args, span: Span):
     package_name: str = to_wrap.get('package')
     if isinstance(wrapped_args, str):
         span.add_event(PROMPT_OUTPUT_KEY, {RESPONSE: wrapped_args})
+    if isinstance(wrapped_args, dict):
+        span.add_event(PROMPT_OUTPUT_KEY, {RESPONSE: wrapped_args['answer']})
     if "llama_index.core.base.base_query_engine" in package_name:
         span.add_event(PROMPT_OUTPUT_KEY, {RESPONSE: wrapped_args.response})
     if "haystack.core.pipeline.pipeline" in package_name:

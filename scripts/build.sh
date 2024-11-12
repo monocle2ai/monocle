1c742c9faf8253948002cbabc9df66aa4e47c6be
sachintendulkar576123 <sachin.tendulkar@beehyv.com>
2024-11-12 15:05:02 +0530

    update prepare-release-branch
    Signed-off-by: sachintendulkar576123 <sachin.tendulkar@beehyv.com>

---
+++ scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# This script builds wheels for the API, SDK, and extension packages in the
+# dist/ dir, to be uploaded to PyPI.
+
+set -ev
+
+# Get the latest versions of packaging tools
+python3 -m pip install --upgrade pip build setuptools wheel
+
+BASEDIR=$(dirname "$(readlink -f "$(dirname $0)")")
+DISTDIR=dist
+
+(
+  cd $BASEDIR
+  mkdir -p $DISTDIR
+  rm -rf ${DISTDIR:?}/*
+
+ for d in opentelemetry-sdk/; do
+   (
+     echo "building $d"
+     cd "$d"
+     # Some ext directories (such as docker tests) are not intended to be
+     # packaged. Verify the intent by looking for a pyproject.toml.
+     if [ -f pyproject.toml ]; then
+      python3 -m build --outdir "$BASEDIR/dist/"
+     fi
+   )
+ done
+)

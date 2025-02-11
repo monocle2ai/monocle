1f19e210a28b7106c9ddfc3e0181b4337b313f21
Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>
2025-02-11 18:46:29 +0530

    added supoprt for prefix
    Signed-off-by: Kshitiz Vijayvargiya <kshitiz.vijayvargiya@okahu.ai>

--- src/monocle_apptrace/exporters/aws/s3_exporter.py
+++ src/monocle_apptrace/exporters/aws/s3_exporter.py
@@ -43,7 +43,7 @@ class S3SpanExporter(SpanExporterBase):
                 region_name=region_name,
             )
         self.bucket_name = bucket_name or os.getenv('MONOCLE_S3_BUCKET_NAME','default-bucket')
-        self.file_prefix = DEFAULT_FILE_PREFIX
+        self.file_prefix = os.getenv('MONOCLE_S3_KEY_PREFIX', DEFAULT_FILE_PREFIX)
         self.time_format = DEFAULT_TIME_FORMAT
         self.export_queue = []
         self.last_export_time = time.time()

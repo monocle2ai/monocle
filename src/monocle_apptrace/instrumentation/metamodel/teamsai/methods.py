dc476c93a60dbf822a72458b0a5a804b40bb9471
Prasad Mujumdar <prasad@okahu.ai>
2025-06-01 22:27:53 -0700

    Update teams scopes, set scope vars directly under the null/exists check
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/instrumentation/metamodel/teamsai/methods.py
+++ src/monocle_apptrace/instrumentation/metamodel/teamsai/methods.py
@@ -19,43 +19,32 @@ def get_id(args, kwargs):
     if context and context.activity and context.activity.channel_id:
         channel_id = context.activity.channel_id or ""
         if channel_id == "msteams":
-            type_name = context.activity.type or ""
- 
-            if hasattr(context.activity,"conversation"):
-                conversation_id = context.activity.conversation.id or ""
-                converstion_type = context.activity.conversation.conversation_type or ""
-                conversation_name = context.activity.conversation.name or ""
-            if hasattr(context.activity,"from_property"):
-                user_teams_id = context.activity.from_property.id or ""
-                user_teams_name = context.activity.from_property.name or ""
-                user_teams_role = context.activity.from_property.role or ""
-            if hasattr(context.activity,"recipient"):
-                recipient_id = context.activity.recipient.id or ""
+            scopes[f"msteams.activity.type"] = context.activity.type or ""
 
-            scopes[f"msteams.activity.type"] = type_name
-            scopes[f"msteams.conversation.id"] = conversation_id
-            scopes[f"msteams.conversation.type"] = converstion_type
-            scopes[f"msteams.conversation.name"] = conversation_name
-            scopes[f"msteams.user.from_property.id"] = user_teams_id
-            scopes[f"msteams.user.from_property.name"] = user_teams_name
-            scopes[f"msteams.user.from_property.role"] = user_teams_role
-            scopes[f"msteams.recipient.id"] = recipient_id
+            if hasattr(context.activity,"conversation"):
+                scopes[f"msteams.conversation.id"] = context.activity.conversation.id or ""
+                scopes[f"msteams.conversation.type"] = context.activity.conversation.conversation_type or ""
+                scopes[f"msteams.conversation.name"] = context.activity.conversation.name or ""
+
+            if hasattr(context.activity,"from_property"):
+                scopes[f"msteams.user.from_property.id"] = context.activity.from_property.id or ""
+                scopes[f"msteams.user.from_property.name"] = context.activity.from_property.name or ""
+                scopes[f"msteams.user.from_property.role"] = context.activity.from_property.role or ""
+
+            if hasattr(context.activity,"recipient"):
+                scopes[f"msteams.recipient.id"] = context.activity.recipient.id or ""
+
             if hasattr(context.activity,"channel_data"):
                 if "tenant" in context.activity.channel_data:
-                    tenant_id = context.activity.channel_data['tenant']['id'] or ""
-                    scopes[f"msteams.channel_data.tenant.id"] = tenant_id
+                    scopes[f"msteams.channel_data.tenant.id"] = context.activity.channel_data['tenant']['id'] or ""
                 if "team" in context.activity.channel_data:
-                    team_id = context.activity.channel_data['team']['id'] or ""
-                    scopes[f"msteams.channel_data.team.id"] = team_id
+                    scopes[f"msteams.channel_data.team.id"] = context.activity.channel_data['team']['id'] or ""
                     if "name" in context.activity.channel_data['team']:
-                        team_name = context.activity.channel_data['team']['name'] or ""
-                        scopes[f"msteams.channel_data.team.name"] = team_name
+                        scopes[f"msteams.channel_data.team.name"] = context.activity.channel_data['team']['name'] or ""
                 if "channel" in context.activity.channel_data:
-                    team_channel_id = context.activity.channel_data['channel']['id'] or ""
-                    scopes[f"msteams.channel_data.channel.id"] = team_channel_id
+                    scopes[f"msteams.channel_data.channel.id"] = context.activity.channel_data['channel']['id'] or ""
                     if "name" in context.activity.channel_data['channel']:
-                        team_channel_name = context.activity.channel_data['channel']['name'] or ""
-                        scopes[f"msteams.channel_data.channel.name"] = team_channel_name
+                        scopes[f"msteams.channel_data.channel.name"] = context.activity.channel_data['channel']['name'] or ""
     return scopes
 
 TEAMAI_METHODS = [

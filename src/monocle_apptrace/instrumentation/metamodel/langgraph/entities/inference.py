cf5a7e917a18dd7d84575c558458abf7e04deaf9
Prasad Mujumdar <prasad@okahu.ai>
2025-06-24 14:04:19 -0700

    Updated agent metamodel for langgraph
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/instrumentation/metamodel/langgraph/entities/inference.py
+++ src/monocle_apptrace/instrumentation/metamodel/langgraph/entities/inference.py
@@ -1,24 +1,19 @@
 from monocle_apptrace.instrumentation.metamodel.langgraph import (
     _helper
 )
-INFERENCE = {
+AGENT = {
       "type": "agent",
       "attributes": [
         [
               {
                 "_comment": "agent type",
                 "attribute": "type",
-                "accessor": lambda arguments:'agent.oai'
+                "accessor": lambda arguments:'agent.langgraph'
               },
               {
                 "_comment": "name of the agent",
                 "attribute": "name",
                 "accessor": lambda arguments:arguments['instance'].name
-              },
-              {
-                "_comment": "agent tools",
-                "attribute": "tools",
-                "accessor": lambda arguments: _helper.tools(arguments['instance'])
               }
         ]
       ],
@@ -27,7 +22,7 @@ INFERENCE = {
           "name":"data.input",
           "attributes": [
             {
-                "_comment": "this is LLM input",
+                "_comment": "this is Agent input",
                 "attribute": "query",
                 "accessor": lambda arguments: _helper.extract_input(arguments)
             }
@@ -42,15 +37,56 @@ INFERENCE = {
                 "accessor": lambda arguments: _helper.handle_openai_response(arguments['result'])
             }
           ]
-       },
-       {
-            "name": "metadata",
-            "attributes": [
-                {
-                    "_comment": "this is metadata usage from LLM",
-                    "accessor": lambda arguments: _helper.update_span_from_llm_response(arguments['result'])
-                }
-            ]
         }
       ]
     }
+
+TOOLS = {
+      "type": "agent.tool",
+      "attributes": [
+        [
+              {
+                "_comment": "tool type",
+                "attribute": "type",
+                "accessor": lambda arguments:'tool.langgraph'
+              },
+              {
+                "_comment": "name of the tool",
+                "attribute": "name",
+                "accessor": lambda arguments:arguments['instance'].name
+              },
+              {
+                  "_comment": "tool description",
+                  "attribute": "description",
+                  "accessor": lambda arguments: arguments['instance'].description
+              }
+        ]
+      ],
+      "events": [
+        {
+          "name":"data.input",
+          "attributes": [
+            {
+                "_comment": "this is Tool input",
+                "attribute": "Inputs",
+                "accessor": lambda arguments: _helper.get_tool_args(arguments) 
+            }
+          ]
+        },
+        {
+          "name":"data.output",
+          "attributes": [
+            {
+                "_comment": "this is response from Tool",
+                "attribute": "response",
+                "accessor": lambda arguments: _helper.extract_response(arguments['result'])
+            },
+            {
+                "_comment": "this is status from Tool",
+                "attribute": "status",
+                "accessor": lambda arguments: _helper.get_status(arguments['result'])
+            }
+          ]
+        }
+      ]
+}

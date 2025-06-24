35842fefd2cf67d44d1b5f9aa0bf8ce698244df4
Prasad Mujumdar <prasad@okahu.ai>
2025-06-24 14:04:19 -0700

    Updated agent metamodel for langgraph
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- src/monocle_apptrace/instrumentation/metamodel/langgraph/methods.py
+++ src/monocle_apptrace/instrumentation/metamodel/langgraph/methods.py
@@ -1,14 +1,21 @@
 from monocle_apptrace.instrumentation.common.wrapper import task_wrapper
 from monocle_apptrace.instrumentation.metamodel.langgraph.entities.inference import (
-    INFERENCE,
+    AGENT, TOOLS
 )
 LANGGRAPH_METHODS = [
+#TODO: Add async methods
     {
         "package": "langgraph.graph.state",
          "object": "CompiledStateGraph",
          "method": "invoke",
-         "span_name": "langgraph.graph.invoke",
          "wrapper_method": task_wrapper,
-         "output_processor": INFERENCE
+         "output_processor": AGENT
+    },
+    {
+        "package": "langchain_core.tools.base",
+         "object": "BaseTool",
+         "method": "run",
+         "wrapper_method": task_wrapper,
+         "output_processor": TOOLS
     }
 ]

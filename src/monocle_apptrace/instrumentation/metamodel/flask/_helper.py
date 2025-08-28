7e108ff336f3e33fa288441a39c7a654a0957e8c
sachintendulkar576123 <sachin.tendulkar@beehyv.com>
2025-08-28 15:19:22 +0530

    Add Http Span for Azfunc and Flask
    Signed-off-by: sachintendulkar576123 <sachin.tendulkar@beehyv.com>

--- src/monocle_apptrace/instrumentation/metamodel/flask/_helper.py
+++ src/monocle_apptrace/instrumentation/metamodel/flask/_helper.py
@@ -23,15 +23,12 @@ def get_params(args) -> dict:
     return unquote(params)
 
 def get_url(args) -> str:
-    try:
-        url = ""
-        if len(args) > 1 or not isinstance(args[0], dict):
-            if 'HTTP_HOST' in args[0]:
-                url = f"http://{args[0]['HTTP_HOST']}{args[0].get('REQUEST_URI', '')}"
+    url = ""
+    if len(args) > 1 or not isinstance(args[0], dict):
+        if 'HTTP_HOST' in args[0]:
+            url = f"http://{args[0]['HTTP_HOST']}{args[0].get('REQUEST_URI', '')}"
 
-        return url
-    except Exception as e:
-        logger.warning(f"Error extracting URL: {e}")
+    return url
 
 def get_body(args) -> dict:
     return ""

7d98ed0405c418a669721671226f96c38b6e29fa
devikapillai30 <160810936+devikapillai30@users.noreply.github.com>
2025-09-08 11:38:15 -0400

    adding mistral metamodel - instrumentation of inference api (#273)
    * mistral metamodel
    
    Signed-off-by: Devika Pillai <devika@Devikas-MacBook-Air.local>
    
    * Added async for chat
    
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    
    * Updated common utils file
    
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    
    ---------
    
    Signed-off-by: Devika Pillai <devika@Devikas-MacBook-Air.local>
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    Co-authored-by: Devika Pillai <devika@Devikas-MacBook-Air.local>
    Co-authored-by: prasad-okahu <147112411+prasad-okahu@users.noreply.github.com>

--- src/monocle_apptrace/instrumentation/common/utils.py
+++ src/monocle_apptrace/instrumentation/common/utils.py
@@ -98,6 +98,7 @@ def with_tracer_wrapper(func):
 
     return _with_tracer
 
+
 def resolve_from_alias(my_map, alias):
     """Find a alias that is not none from list of aliases"""
 
@@ -393,12 +394,14 @@ def get_exception_message(arguments):
     else:
         return ''
 
+
 def get_error_message(arguments):
     status_code = get_status_code(arguments)
     if status_code == 'success':
         return ''
     else:
         return status_code
+
 
 def get_status_code(arguments):
     if arguments["exception"] is not None:

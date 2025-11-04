4a61857920292651f414be9265a44f5198d4eeed
sachintendulkar576123 <sachin.tendulkar@beehyv.com>
2025-11-04 23:23:37 +0530

    Add tool_call name and type in inference span
    Signed-off-by: sachintendulkar576123 <sachin.tendulkar@beehyv.com>

--- apptrace/src/monocle_apptrace/instrumentation/metamodel/langchain/entities/inference.py
+++ apptrace/src/monocle_apptrace/instrumentation/metamodel/langchain/entities/inference.py
@@ -36,8 +36,20 @@ INFERENCE = {
             },
             {
                 "attribute": "type",
-                "accessor": lambda arguments: 'model.llm.' + resolve_from_alias(arguments['instance'].__dict__, ['model', 'model_name', 'endpoint_name', 'deployment_name', 'model_id'])
+                "accessor": lambda arguments: 'model.llm.' + resolve_from_alias(arguments['kwargs'], ['model', 'model_name', 'endpoint_name', 'deployment_name'])
             }
+        ],
+        [
+            {
+                "_comment": "Tool name when finish_type is tool_call",
+                "attribute": "name",
+                "accessor": lambda arguments: _helper.extract_tool_name(arguments),
+            },
+            {
+                "_comment": "Tool type when finish_type is tool_call", 
+                "attribute": "type",
+                "accessor": lambda arguments: _helper.extract_tool_type(arguments),
+            },
         ]
     ],
     "events": [

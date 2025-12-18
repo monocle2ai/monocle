2a0c53fba2e81983f9ebd44a2009121a31996cd1
prasad-okahu <147112411+prasad-okahu@users.noreply.github.com>
2025-12-19 06:13:47 +0900

    Fluent test API improvements (#380)
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- test_tools/tests/unit/test_fluent_apis.py
+++ test_tools/tests/unit/test_fluent_apis.py
@@ -2,14 +2,10 @@ import pytest
 import os
 from monocle_test_tools import TraceAssertion
 from span_loader import JSONSpanLoader
+os.environ["MONOCLE_EXPORT_FAILED_TESTS_ONLY"] = "true"
 
-def _load_spans(validator: TraceAssertion):
-    current_script_path = os.path.abspath(__file__)
-    spans = JSONSpanLoader.from_json(os.path.join(os.path.dirname(current_script_path), "traces/trace1.json"))
-    validator.memory_exporter.export(spans)
-
-def test_tool_invocation_span(monocle_trace_asserter):
-    _load_spans(monocle_trace_asserter)
+def test_tool_invocation_span(monocle_trace_asserter:TraceAssertion):
+    monocle_trace_asserter.load_spans(JSONSpanLoader.load_spans("traces/trace1.json"))
     monocle_trace_asserter.called_tool("adk_book_hotel_5", "adk_hotel_booking_agent_5") \
         .has_input("{'city': 'Mumbai', 'hotel_name': 'Marriot Intercontinental'}") \
         .has_output("{'status': 'success', 'message': 'Successfully booked a stay at Marriot Intercontinental in Mumbai.'}") \
@@ -18,11 +14,11 @@ def test_tool_invocation_span(monocle_trace_asserter):
         .does_not_contain_input("Delhi") \
         .does_not_contain_output("failed")
 
-def test_agent_invocation(monocle_trace_asserter):
-    _load_spans(monocle_trace_asserter)
+def test_agent_invocation(monocle_trace_asserter:TraceAssertion):
+    monocle_trace_asserter.load_spans(JSONSpanLoader.load_spans("traces/trace1.json"))
     monocle_trace_asserter.called_agent("adk_hotel_booking_agent_5") \
-        .has_input("Book a two queen room at Marriot Intercontinental at Juhu, Mumbai for 27th Nov 2025 for 4 nights.") \
-        .contains_output("book a flight from San Francisco to Mumbai for 26th Nov 2025") \
+        .has_input("Book a flight from San Francisco to Mumbai for 26th Nov 2025. Book a two queen room at Marriot Intercontinental at Juhu, Mumbai for 27th Nov 2025 for 4 nights.") \
+        .contains_output("I have booked a stay at Marriot Intercontinental in Mumbai.") \
         .does_not_have_output("cancel the booking") \
         .does_not_have_output("failed")
 

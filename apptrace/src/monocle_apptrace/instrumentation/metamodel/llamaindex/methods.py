0a097df9bf7918aa5f051dedf3843b11fb5d3817
divyam-v <divyam.v@beehyv.com>
2025-12-30 22:00:28 +0530

    Fix wrapper method for AgentWorkflow in LLAMAINDEX_METHODS (#393)
    Signed-off-by: divyam-v <divyam.v@beehyv.com>

--- apptrace/src/monocle_apptrace/instrumentation/metamodel/llamaindex/methods.py
+++ apptrace/src/monocle_apptrace/instrumentation/metamodel/llamaindex/methods.py
@@ -83,7 +83,7 @@ LLAMAINDEX_METHODS = [
         "object": "AgentWorkflow",
         "method": "run",
         "span_handler": "llamaindex_agent_handler",
-        "wrapper_method": task_wrapper,
+        "wrapper_method": atask_wrapper,
         "output_processor": AGENT_REQUEST
     },
     {

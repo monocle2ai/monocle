57d4583078f307f71eedd617beb224811ab3eb0c
Prasad Mujumdar <prasad@okahu.ai>
2026-02-13 14:35:03 -0800

    Ensure that the fluent trace asserter has full set of spans at start of every chain
    Signed-off-by: Prasad Mujumdar <prasad@okahu.ai>

--- test_tools/tests/integration/test_adk_travel_agent_fluent.py
+++ test_tools/tests/integration/test_adk_travel_agent_fluent.py
@@ -9,6 +9,7 @@ async def test_tool_invocation(monocle_trace_asserter):
     await monocle_trace_asserter.run_agent_async(root_agent, "google_adk", 
                         "Book a flight from San Francisco to Mumbai for 26th Nov 2025")
     monocle_trace_asserter.called_tool("adk_book_flight_5","adk_flight_booking_agent_5").contains_input("Mumbai")
+    monocle_trace_asserter.under_token_limit(1000000)
 
 @pytest.mark.asyncio
 async def test_agent_invocation(monocle_trace_asserter):

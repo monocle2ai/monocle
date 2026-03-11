100603e0b755edf5c3bb0750b51b9b81155b3588
shambhavigoel-13 <shambhavi.goyal@okahu.ai>
2026-03-11 09:05:21 +0530

    Monocle Test Execution in Jenkins (#479)
    * Monocle Test Execution in Jenkins
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * git ignore change
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * removed the comments on conftest
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * formatted validator file.
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * sample trace with jnekins added
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * Added input and output in FastApi, Flask, Aiohhtp for POST method (#481)
    
    Signed-off-by: sachintendulkar576123 <sachin.tendulkar@beehyv.com>
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * Implement duplicate setup detection for telemetry configuration and add integration tests (#478)
    
    Signed-off-by: divyam-v <divyam.v@beehyv.com>
    Co-authored-by: prasad-okahu <147112411+prasad-okahu@users.noreply.github.com>
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * Changes for Crew eval (#473)
    
    * Crew eval changes
    
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    
    * moved code to base exporter
    
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    
    * changes acc to comment
    
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    
    ---------
    
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    Co-authored-by: prasad-okahu <147112411+prasad-okahu@users.noreply.github.com>
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    * updated validator with missed changes
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    
    ---------
    
    Signed-off-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    Signed-off-by: sachintendulkar576123 <sachin.tendulkar@beehyv.com>
    Signed-off-by: divyam-v <divyam.v@beehyv.com>
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>
    Co-authored-by: shambhavigoel-13 <shambhavi.goyal@okahu.in>
    Co-authored-by: sachin576 <sachin.tendulkar@beehyv.com>
    Co-authored-by: divyam-v <divyam.v@beehyv.com>
    Co-authored-by: prasad-okahu <147112411+prasad-okahu@users.noreply.github.com>
    Co-authored-by: devikapillai30 <160810936+devikapillai30@users.noreply.github.com>

--- test_tools/src/monocle_test_tools/constants.py
+++ test_tools/src/monocle_test_tools/constants.py
@@ -1,7 +1,30 @@
-TEST_SCOPE_NAME:str = "test_name"
+TEST_SCOPE_NAME: str = "test_name"
 GIT_RUN_ID_ATTRIBUTE: str = "git.run.id"
 GIT_WORKFLOW_NAME_ATTRIBUTE: str = "git.workflow.name"
 GIT_COMMIT_HASH_ATTRIBUTE: str = "git.commit.hash"
 DEFAULT_WORKFLOW_NAME: str = "monocle_validator"
 TEST_STATUS_ATTRIBUTE: str = "test.status"
 TEST_ASSERTION_ATTRIBUTE: str = "test.assertion.message"
+
+# Jenkins environment variables
+JENKINS_BUILD_NUMBER: str = "BUILD_NUMBER"
+JENKINS_BUILD_ID: str = "BUILD_ID"
+JENKINS_JOB_NAME: str = "JOB_NAME"
+JENKINS_BUILD_URL: str = "BUILD_URL"
+JENKINS_URL: str = "JENKINS_URL"
+JENKINS_NODE_NAME: str = "NODE_NAME"
+JENKINS_GIT_COMMIT: str = "GIT_COMMIT"
+JENKINS_GIT_BRANCH: str = "GIT_BRANCH"
+
+# Jenkins trace attributes
+JENKINS_URL_ATTRIBUTE: str = "jenkins.url"
+JENKINS_BUILD_URL_ATTRIBUTE: str = "jenkins.build.url"
+JENKINS_NODE_NAME_ATTRIBUTE: str = "jenkins.node.name"
+
+# GitHub Actions environment variables (original)
+GITHUB_WORKFLOW: str = "GITHUB_WORKFLOW"
+GITHUB_RUN_ID: str = "GITHUB_RUN_ID"
+GITHUB_SHA: str = "GITHUB_SHA"
+
+# Local environment
+LOCAL_RUN_ID: str = "LOCAL_RUN_ID"

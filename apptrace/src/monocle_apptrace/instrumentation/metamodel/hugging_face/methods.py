5bc2f21bf6877801203e8812055cb976835e4f65
devikapillai30 <devikapillai145@gmail.com>
2026-04-01 22:18:28 -0400

    version update fixes
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>

--- apptrace/src/monocle_apptrace/instrumentation/metamodel/hugging_face/methods.py
+++ apptrace/src/monocle_apptrace/instrumentation/metamodel/hugging_face/methods.py
@@ -1,4 +1,9 @@
 from monocle_apptrace.instrumentation.common.wrapper import atask_wrapper, task_wrapper
+from monocle_apptrace.instrumentation.metamodel.hugging_face.entities.agent import (
+    AGENT,
+    AGENT_REQUEST,
+    TOOL,
+)
 from monocle_apptrace.instrumentation.metamodel.hugging_face.entities.inference import (
     INFERENCE,
 )
@@ -19,5 +24,37 @@ HUGGING_FACE_METHODS = [
         "wrapper_method": atask_wrapper,
         "span_handler": "non_framework_handler",
         "output_processor": INFERENCE,
+    },
+    {
+        "package": "smolagents.agents",
+        "object": "MultiStepAgent",
+        "method": "run",
+        "wrapper_method": task_wrapper,
+        "span_handler": "non_framework_handler",
+        "output_processor": AGENT_REQUEST,
+    },
+    {
+        "package": "smolagents.agents",
+        "object": "CodeAgent",
+        "method": "run",
+        "wrapper_method": task_wrapper,
+        "span_handler": "non_framework_handler",
+        "output_processor": AGENT,
+    },
+    {
+        "package": "smolagents.agents",
+        "object": "ToolCallingAgent",
+        "method": "run",
+        "wrapper_method": task_wrapper,
+        "span_handler": "non_framework_handler",
+        "output_processor": AGENT,
+    },
+    {
+        "package": "smolagents.tools",
+        "object": "Tool",
+        "method": "__call__",
+        "wrapper_method": task_wrapper,
+        "span_handler": "non_framework_handler",
+        "output_processor": TOOL,
     },
 ]

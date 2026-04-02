481da9234de2fcc6e66f73ba2f8c7b9bd24b4c52
devikapillai30 <devikapillai145@gmail.com>
2026-04-02 09:28:12 -0400

    anthropic streaming
    Signed-off-by: devikapillai30 <devikapillai145@gmail.com>

--- apptrace/src/monocle_apptrace/instrumentation/metamodel/anthropic/methods.py
+++ apptrace/src/monocle_apptrace/instrumentation/metamodel/anthropic/methods.py
@@ -1,6 +1,7 @@
 from monocle_apptrace.instrumentation.common.wrapper import atask_wrapper, task_wrapper
 from monocle_apptrace.instrumentation.metamodel.anthropic.entities.inference import (
     INFERENCE,
+    STREAM_INFERENCE,
 )
 
 ANTHROPIC_METHODS = [
@@ -14,11 +15,27 @@ ANTHROPIC_METHODS = [
     },
     {
         "package": "anthropic.resources",
+        "object": "Messages",
+        "method": "stream",
+        "wrapper_method": task_wrapper,
+        "span_handler": "non_framework_handler",
+        "output_processor": STREAM_INFERENCE
+    },
+    {
+        "package": "anthropic.resources",
         "object": "AsyncMessages",
         "method": "create",
         "wrapper_method": atask_wrapper,
         "span_handler": "non_framework_handler",
         "output_processor": INFERENCE
+    },
+    {
+        "package": "anthropic.resources",
+        "object": "AsyncMessages",
+        "method": "stream",
+        "wrapper_method": atask_wrapper,
+        "span_handler": "non_framework_handler",
+        "output_processor": STREAM_INFERENCE
     },
 
 ]
